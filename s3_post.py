65d52e46fc6aa1be17a80029a6c3b0613dbd5811
Nick Barendt <nbarendt@ubuntu.(none)>
2010-10-22 11:05:12 -0400

    cleaned up err message on missing config file

--- s3_post.py
+++ s3_post.py
@@ -7,7 +7,8 @@ CONFIG_FILE='server.ini'
 def get_s3_connection():
     # read the AWS S3 Credentials in from the ini file
     config = SafeConfigParser()
-    assert os.path.exists(CONFIG_FILE), 'Required file %s not found!' % CONFIG_FILE
+    err_msg = 'Required file %s not found!' % CONFIG_FILE
+    assert os.path.exists(CONFIG_FILE), err_msg
     config.read(CONFIG_FILE)
     # create an S3 Boto connection
     return connect_s3(config.get('s3', 'access'), config.get('s3', 'secret'))
@@ -17,8 +18,6 @@ def get_post_args(bucket, key):
             bucket_name=bucket,
             key=key,
             expires_in=6000,
-            #success_action_redirect='http://{0}:{1}/upload_complete'.format(
-            #    self.server.server_name, self.server.server_port),
             fields=[{'name': 'success_action_status',
                      'value': 201}],
             conditions=["['starts-with', '$Filename', '']",
